9243c41d6b7bb3acc8c5bfeb13429c2ebc0de49f
Jack Deeth <developer@jackdeeth.org.uk>
2023-10-28 13:13:40 +0100

    Remove test dependency on {user}/Saved Games/DCSZap folder existing

--- tests/test_socket.py
+++ tests/test_socket.py
@@ -1,11 +1,12 @@
 # pylint: skip-file
+import os
 from dcszap import App
 
 
 def test_send_command(mocker):
     mock_socket = mocker.patch("dcszap.socket.socket", autospec=True).return_value
 
-    app = App("192.1.2.3", 24601)
+    app = App("192.1.2.3", 24601, os.curdir)
     app.send_cmd("PANEL_LIGHTING", 1)
 
     mock_socket.sendto.assert_called_once_with(
